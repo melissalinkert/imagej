d4433f635429956ac74e410520b17566180ac8f3
dscho <dscho@be23795f-a60e-40d4-ba19-055798872e57>
2006-10-03 00:42:34 +0000

    sync with 1.37t
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@156 be23795f-a60e-40d4-ba19-055798872e57

--- ij/io/Opener.java
+++ ij/io/Opener.java
@@ -52,7 +52,8 @@ public class Opener {
 			String path = directory+name;
 			error = false;
 			open(path);
-			if (!error) Menus.addOpenRecentItem(path);
+			if (!error)
+				record(path);
 		}
 	}
 
@@ -162,6 +163,8 @@ public class Opener {
 					break;
 			}
 		}
+		if(!error)
+			record(path);
 	}
 
 	/** Attempts to open the specified file as a tiff, bmp, dicom, fits,
@@ -699,4 +702,9 @@ public class Opener {
 		silentMode = mode;
 	}
 
+	public void record(String path) {
+		Menus.addOpenRecentItem(path);
+		if(Recorder.record)
+			Recorder.record("open",path);
+	}
 }
