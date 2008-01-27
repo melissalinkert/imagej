48e1b6d6bb70daa006bf13d618aef34cecdefe24
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2008-01-27 03:27:33 +0000

    • 1.39q, 26 January 2008       □ When using the Image>Adjust>Brightness/Contrast tool, hold the shift key         down to simultaneously adjust all channels of a composite image (e.g.,         File>Open Samples>HeLa Cells).       □ Clicking "Apply" in Image>Adjust>Brightness/Contrast propagates the         current composite image display range to the other channels.       □ Image>Adjust>Color Balance works with composite images.       □ The text tool is recorded.       □ ImageJ now opens LZW-compressed RGB planar TIFFs.       □ Stefan Woerz added Gaussian fitting to the CurveFitter class.       □ "Maximize" works better with HyperStacks and with image windows that         have button panels.       □ Albert Cardona added HandleExtraFileTypes support for File>Import>URL.       □ Added the is("Virtual Stack") macro function.       □ Fixed a bug that caused files in folders dropped onto ImageJ to not be         sorted in numeric order.       □ Fixed a bug in File>Import>Raw (ImageReader class) that caused it to not         correctly handle EOF errors with data types other than 8-bit.       □ Fixed a bug in the PGM_Reader that caused pixel intensities of imported         48-bit images to differ slightly from the expected values.
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@258 be23795f-a60e-40d4-ba19-055798872e57

--- ij/macro/Interpreter.java
+++ ij/macro/Interpreter.java
@@ -102,13 +102,12 @@ public class Interpreter implements MacroConstants {
 
 	/** Interprets the specified tokenized macro starting at the specified location. */
 	public void runMacro(Program pgm, int macroLoc, String macroName) {
+		calledMacro = true;
 		this.pgm = pgm;
 		this.macroName = macroName;
 		pc = macroLoc-1;
 		previousInstance = instance;
 		instance = this;
-		batchMode = false;
-		imageTable = null;
 		//IJ.showStatus("interpreting");
 		pushGlobals();
 		if (func==null)
@@ -1609,22 +1608,6 @@ public class Interpreter implements MacroConstants {
 		return (ImagePlus)imageTable.elementAt(size-1); 
 	} 
  
-	public void setLocalVariable(String key,String value) {
-	    Symbol sym=pgm.lookupWord(key);
-	    int symTabAddress;
-	    if(sym==null) {
-		sym=new Symbol(MacroConstants.WORD,key);
-		pgm.addSymbol(sym);
-		symTabAddress=pgm.stLoc-1;
-	    } else
-		symTabAddress=pgm.symTabLoc;
-	    Variable var=lookupLocalVariable(symTabAddress);
-	    if(var==null) {
-		push(symTabAddress, 0.0, value, this);
-	    } else
-		var.setString(value);
-	}
-
  	/** The specified string, if not null, is added to strings passed to the run() method. */
  	public static void setAdditionalFunctions(String functions) {
  		additionalFunctions = functions;
--- ij/plugin/frame/Recorder.java
+++ ij/plugin/frame/Recorder.java
@@ -231,7 +231,14 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 	}
 	
 	static String trimKey(String key) {
-		return Macro.trimKey(key);
+		int index = key.indexOf(" ");
+		if (index>-1)
+			key = key.substring(0,index);
+		index = key.indexOf(":");
+		if (index>-1)
+			key = key.substring(0,index);
+		key = key.toLowerCase(Locale.US);
+		return key;
 	}
 
 	/** Writes the current command and options to the Recorder window. */
@@ -257,11 +264,18 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 				else 
 					textArea.append("run(\""+name+"\", \""+commandOptions+"\");\n");
 			} else {
-				if (name.equals("Threshold..."))
+				if (name.equals("Threshold...") || name.equals("Fonts..."))
 					textArea.append("//run(\""+name+"\");\n");
 				else if (name.equals("Start Animation [\\]"))
 					textArea.append("doCommand(\"Start Animation [\\\\]\");\n");
-				else {
+				else if (name.equals("Draw")) {
+					ImagePlus imp = WindowManager.getCurrentImage();
+					Roi roi = imp.getRoi();
+					if (roi!=null && (roi instanceof TextRoi))
+						textArea.append(((TextRoi)roi).getMacroCode(imp.getProcessor()));
+					else
+						textArea.append("run(\""+name+"\");\n");
+				} else {
 					if (IJ.altKeyDown() && (name.equals("Open Next")||name.equals("Plot Profile")))
 						textArea.append("setKeyDown(\"alt\"); ");
 					textArea.append("run(\""+name+"\");\n");
@@ -273,11 +287,22 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 	}
 	
 	static boolean isSaveAs() {
-		Menu saveAsMenu = Menus.getSaveAsMenu();
-		for(int i=0;i<saveAsMenu.getItemCount();i++)
-			if(commandName.equals(saveAsMenu.getItem(i).getLabel()))
-				return true;
-		return false;
+		return commandName.equals("Tiff...")
+			|| commandName.equals("Gif...")
+			|| commandName.equals("Jpeg...")
+			|| commandName.equals("Text Image...")
+			|| commandName.equals("ZIP...")
+			|| commandName.equals("Raw Data...")
+			|| commandName.equals("AVI... ")
+			|| commandName.equals("BMP...")
+			|| commandName.equals("PNG...")
+			|| commandName.equals("PGM...")
+			|| commandName.equals("FITS...")
+			|| commandName.equals("LUT...")
+			|| commandName.equals("Selection...")
+			|| commandName.equals("XY Coordinates...")
+			|| commandName.equals("Measurements...")
+			|| commandName.equals("Text... ");
 	}
 
 	static void appendNewImage() {
@@ -365,7 +390,7 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
     
     public void windowClosing(WindowEvent e) {
     	close();
-    }
+	}
 
 	public void close() {
 		super.close();
