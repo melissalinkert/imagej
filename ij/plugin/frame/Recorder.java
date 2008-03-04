7e2bcd60765eeb32d0de57c33309b7a28f5253b0
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2008-03-04 13:47:43 +0000

    • 1.39t, 3 March 2008       □ The Image>Adjust>Brightness/Contrast histogram is now updated when the         B&C window is brought to the front.       □ The Analyze>Gels>Select First Lane command displays a "Are the lanes         really horizontal?" dialog when the selection is more than twice as         wide as it is tall.       □ An error message is displayed when a calibrated image is opened and         "Global" calibration is enabled.       □ PNGs are again opened using ImageIO, which fixes several bugs         (introduced in v1.39i) that caused 16-bit and RGB PNGs to not open or         to not open correctly.       □ Added the getInfo("image.description") macro function.       □ Added the ImportDialog.getLastFileInfo() method.       □ Fixed a bug that caused File>Import>URL to fail to open GIF, JPEG, PNG         and DICOM files with uppercase extensions.       □ Fixed several bugs in Edit>Selection>Specify, thanks to patches         contributed by Joachim Wesner.       □ Fixed a bug in Image>Scale that could cause a blank result if the         source was RGB, the x scale was less than 0.25 and the y scale was         greater than 0.5.
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@269 be23795f-a60e-40d4-ba19-055798872e57

--- ij/plugin/frame/Recorder.java
+++ ij/plugin/frame/Recorder.java
@@ -231,7 +231,14 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 	}
 	
 	static String trimKey(String key) {
-		return Macro.trimKey(key);
+		int index = key.indexOf(" ");
+		if (index>-1)
+			key = key.substring(0,index);
+		index = key.indexOf(":");
+		if (index>-1)
+			key = key.substring(0,index);
+		key = key.toLowerCase(Locale.US);
+		return key;
 	}
 
 	/** Writes the current command and options to the Recorder window. */
@@ -280,11 +287,22 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 	}
 	
 	static boolean isSaveAs() {
-		Menu saveAsMenu = Menus.getSaveAsMenu();
-		for(int i=0;i<saveAsMenu.getItemCount();i++)
-			if(commandName.equals(saveAsMenu.getItem(i).getLabel()))
-				return true;
-		return false;
+		return commandName.equals("Tiff...")
+			|| commandName.equals("Gif...")
+			|| commandName.equals("Jpeg...")
+			|| commandName.equals("Text Image...")
+			|| commandName.equals("ZIP...")
+			|| commandName.equals("Raw Data...")
+			|| commandName.equals("AVI... ")
+			|| commandName.equals("BMP...")
+			|| commandName.equals("PNG...")
+			|| commandName.equals("PGM...")
+			|| commandName.equals("FITS...")
+			|| commandName.equals("LUT...")
+			|| commandName.equals("Selection...")
+			|| commandName.equals("XY Coordinates...")
+			|| commandName.equals("Measurements...")
+			|| commandName.equals("Text... ");
 	}
 
 	static void appendNewImage() {
@@ -303,8 +321,15 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
 		int index = value.indexOf('=');
 		if (index>=0)
 			value = value.substring(index+1);
-		if (value.startsWith("["))
-			value = value.substring(1, value.length()-1);
+		if (value.startsWith("[")) {
+			int index2 = value.indexOf(']');
+			if (index2==-1) index2 = value.length();
+			value = value.substring(1, index2);
+		} else {
+			index = value.indexOf(' ');
+			if (index!=-1)
+				value = value.substring(0, index);
+		}
 		return value;
 	}
 
@@ -372,7 +397,7 @@ public class Recorder extends PlugInFrame implements PlugIn, ActionListener {
     
     public void windowClosing(WindowEvent e) {
     	close();
-    }
+	}
 
 	public void close() {
 		super.close();
