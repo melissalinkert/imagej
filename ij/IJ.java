80c6df18b0b7f622ef17e2a1e601406d3e64075d
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2009-03-06 23:03:48 +0000

    Sync with ImageJ 1.42k
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@579 be23795f-a60e-40d4-ba19-055798872e57

--- ij/IJ.java
+++ ij/IJ.java
@@ -68,6 +68,8 @@ public class IJ {
 	}
 			
 	static void init(ImageJ imagej, Applet theApplet) {
+		if (theApplet == null)
+			System.setSecurityManager(null);
 		ij = imagej;
 		applet = theApplet;
 		progressBar = ij.getProgressBar();
@@ -184,7 +186,17 @@ public class IJ {
 		}
 		catch (NoClassDefFoundError e) {
 			int dotIndex = className.indexOf('.');
-			if (dotIndex >= 0)
+			String cause = e.getMessage();
+			int parenIndex = cause.indexOf('(');
+			if (parenIndex >= 1)
+				cause = cause.substring(0, parenIndex - 1);
+			boolean correctClass = cause.endsWith(dotIndex < 0 ?
+				className : className.substring(dotIndex + 1));
+			if (!correctClass && !suppressPluginNotFoundError)
+				error("Plugin " + className +
+					" did not find required class: " +
+					e.getMessage());
+			if (correctClass && dotIndex >= 0)
 				return runUserPlugIn(commandName, className.substring(dotIndex + 1), arg, createNewLoader);
 			if (className.indexOf('_')!=-1 && !suppressPluginNotFoundError)
 				error("Plugin or class not found: \"" + className + "\"\n(" + e+")");
@@ -486,7 +498,7 @@ public class IJ {
 		macro is running, it is aborted. Writes to the Java console
 		if the ImageJ window is not present.*/
 	public static void error(String msg) {
-		showMessage("ImageJ", msg);
+		showMessage("ImageJA", msg);
 		if (Thread.currentThread().getName().endsWith("JavaScript"))
 			throw new RuntimeException(Macro.MACRO_CANCELED);
 		else
@@ -794,7 +806,7 @@ public class IJ {
 	public static boolean versionLessThan(String version) {
 		boolean lessThan = ImageJ.VERSION.compareTo(version)<0;
 		if (lessThan)
-			error("This plugin or macro requires ImageJ "+version+" or later.");
+			error("This plugin or macro requires ImageJA "+version+" or later.");
 		return lessThan;
 	}
 	
@@ -1464,7 +1476,7 @@ public class IJ {
 		if (ij!=null || Interpreter.isBatchMode())
 			throw new RuntimeException(Macro.MACRO_CANCELED);
 	}
-	
+
 	static void setClassLoader(ClassLoader loader) {
 		classLoader = loader;
 	}
