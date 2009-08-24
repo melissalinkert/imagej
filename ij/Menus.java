c4dd256a303a221e809c38e60a4ccdeb8100995c
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2009-08-24 21:49:35 +0000

    • 1.43f, 24 August 2009       □ The Image>Scale and Image>Adjust>Size commands now scale stacks         and hyperstacks in the Z dimension.       □ The Image>Color>Merge Channels command now works with         hyperstacks.       □ Added the File>New>Hyperstack command, an alias for Image>         Hyperstacks>New Hyperstack.       □ Analyze>Tools>Fractal Box Counter writes results that can be         read by macros.       □ Added the List.setCommands, setFont("user"), getInfo         ("font.name"), getValue("font.size") and getValue         ("font.height") macro functions.       □ Thanks to Johannes Schindelin, added the         NonBlockingGenericDialog class, now used by the Process>Batch>         Process command.       □ Fixed a bug that sometimes caused the Image>Adjust>Brightness/         Contrast "Apply" function to fail with RGB stacks.       □ Fixed a bug that sometimes caused Image>Color>Split Channels to         display a "save changes?" dialog box.       □ Fixed a bug that caused File>Save As>Image Sequence to throw an         exception if the window was closed before all the images in the         stack were saved.
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@732 be23795f-a60e-40d4-ba19-055798872e57

--- ij/Menus.java
+++ ij/Menus.java
@@ -8,6 +8,7 @@ import java.awt.image.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+import java.applet.Applet;
 import java.awt.event.*;
 import java.util.zip.*;
 
@@ -49,7 +50,7 @@ public class Menus {
 	private static PopupMenu popup;
 
 	private static ImageJ ij;
-	private static ImageJApplet applet;
+	private static Applet applet;
 	private Hashtable demoImagesTable = new Hashtable();
 	private static String pluginsPath, macrosPath;
 	private static Properties menus;
@@ -78,7 +79,7 @@ public class Menus {
 
 	static boolean jnlp; // true when using Java WebStart
 		
-	Menus(ImageJ ijInstance, ImageJApplet appletInstance) {
+	Menus(ImageJ ijInstance, Applet appletInstance) {
 		ij = ijInstance;
 		applet = appletInstance;
 	}
@@ -87,7 +88,7 @@ public class Menus {
 		nPlugins = nMacros = userPluginsIndex = 0;
 		addSorted = installingJars = duplicateCommand = false;
 		error = null;
-		mbar = applet == null ? null : applet.menu.getMenuBar();
+		mbar = null;
 		menus = new Properties();
 		pluginsTable = new Hashtable();
 		shortcuts = new Hashtable();
@@ -98,8 +99,7 @@ public class Menus {
 		Menu newMenu = getMenu("File>New", true);
 		addPlugInItem(file, "Open...", "ij.plugin.Commands(\"open\")", KeyEvent.VK_O, false);
 		addPlugInItem(file, "Open Next", "ij.plugin.NextImageOpener", KeyEvent.VK_O, true);
-		if (applet == null)
-			getMenu("File>Open Samples", true);
+		getMenu("File>Open Samples", true);
 		addOpenRecentSubMenu(file);
 		Menu importMenu = getMenu("File>Import", true);
 		file.addSeparator();
@@ -149,7 +149,7 @@ public class Menus {
 		getMenu("Image>Stacks", true);
 		Menu hyperstacksMenu = getMenu("Image>Hyperstacks", true);
 		image.addSeparator();
-		addPlugInItem(image, "Crop", "ij.plugin.filter.Resizer(\"crop\")", KeyEvent.VK_X, true);
+		addPlugInItem(image, "Crop", "ij.plugin.Resizer(\"crop\")", KeyEvent.VK_X, true);
 		addPlugInItem(image, "Duplicate...", "ij.plugin.filter.Duplicater", KeyEvent.VK_D, true);
 		addPlugInItem(image, "Rename...", "ij.plugin.SimpleCommands(\"rename\")", 0, false);
 		addPlugInItem(image, "Scale...", "ij.plugin.Scaler", KeyEvent.VK_E, false);
@@ -220,13 +220,12 @@ public class Menus {
 		addPlugInItem(help, "Update Menus", "ij.plugin.ImageJ_Updater(\"menus\")", 0, false);
 		help.addSeparator();
 		Menu aboutMenu = getMenu("Help>About Plugins", true);
-		addPlugInItem(help, "About ImageJA...", "ij.plugin.AboutBoxJA", 0, false);
+		addPlugInItem(help, "About ImageJ...", "ij.plugin.AboutBox", 0, false);
 				
 		if (applet==null) {
 			menuSeparators = new Properties();
 			installPlugins();
 		}
-
 
 		// make	sure "Quit" is the last item in the File menu
 		file.addSeparator();
@@ -234,7 +233,7 @@ public class Menus {
 
 		if (fontSize!=0)
 			mbar.setFont(getFont());
-		if (ij!=null && applet == null)
+		if (ij!=null)
 			ij.setMenuBar(mbar);
 		
 		if (pluginError!=null)
@@ -661,7 +660,7 @@ public class Menus {
 		if (result == null) {
 			int offset = menuName.lastIndexOf('>');
 			if (offset < 0) {
-				result = new PopupMenu(menuName);
+				result = new Menu(menuName);
 				if (mbar == null)
 					mbar = new MenuBar();
 				if (menuName.equals("Help"))
@@ -1206,11 +1205,11 @@ public class Menus {
 	public static PopupMenu getPopupMenu() {
 		return popup;
 	}
-
+	
 	public static Menu getSaveAsMenu() {
 		return saveAsMenu;
 	}
-	
+
 	/** Adds a plugin based command to the end of a specified menu.
 	* @param plugin			the plugin (e.g. "Inverter_", "Inverter_("arg")")
 	* @param menuCode		PLUGINS_MENU, IMPORT_MENU, SAVE_AS_MENU or HOT_KEYS
@@ -1448,8 +1447,7 @@ public class Menus {
 	
 	public static void updateImageJMenus() {
 		jarFiles = macroFiles = null;
-		Menus m = new Menus(IJ.getInstance(),
-				(ImageJApplet)IJ.getApplet());
+		Menus m = new Menus(IJ.getInstance(), IJ.getApplet());
 		String err = m.addMenuBar();
 		if (err!=null) IJ.error(err);
 		IJ.setClassLoader(null);
