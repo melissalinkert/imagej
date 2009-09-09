2416a6c7577ed2448361018747800f1998fdeda1
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2009-09-09 22:38:36 +0000

    updated 10.09.2009 00:38
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@739 be23795f-a60e-40d4-ba19-055798872e57

--- ij/plugin/Compiler.java
+++ ij/plugin/Compiler.java
@@ -141,15 +141,6 @@ public class Compiler implements PlugIn, FilenameFilter {
 		}
 	}
 
-       public static boolean compileFile(String path) {
-	       Compiler compiler = new Compiler();
-               if(!compiler.isJavac())
-                       return false;
-               return compiler.compile(path);
-         
-       }
-
-
 	boolean areErrors(String s) {
 		boolean errors = s!=null && s.length()>0;
 		if(errors && s.indexOf("1 warning")>0 && s.indexOf("[deprecation] show()")>0)
@@ -174,8 +165,7 @@ public class Compiler implements PlugIn, FilenameFilter {
 	 	boolean okay;
 		String fileName, directory;
 	 	if (path.equals("")) {
-			if (dir==null)
-				dir = Prefs.getHomeDir();
+			if (dir==null) dir = IJ.getDirectory("plugins");
 			OpenDialog od = new OpenDialog(msg, dir, name);
 			directory = od.getDirectory();
 			fileName = od.getFileName();
