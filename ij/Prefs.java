60cfd523786c8b797fdaf7c453552fb840850663
wsr <wsr@be23795f-a60e-40d4-ba19-055798872e57>
2009-10-15 20:21:49 +0000

    updated 15.10.2009 (1.43i-7)
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@781 be23795f-a60e-40d4-ba19-055798872e57

--- ij/Prefs.java
+++ ij/Prefs.java
@@ -37,7 +37,6 @@ public class Prefs {
     public static final String NOISE_SD = "noise.sd";
     public static final String MENU_SIZE = "menu.size";
     public static final String THREADS = "threads";
-    public static final String ENABLE_RMI = "enable.rmi.listener";
 	public static final String KEY_PREFIX = ".";
  
 	private static final int USE_POINTER=1<<0, ANTIALIASING=1<<1, INTERPOLATE=1<<2, ONE_HUNDRED_PERCENT=1<<3,
@@ -100,9 +99,7 @@ public class Prefs {
 	public static boolean pointAddToManager;
 	/** Extend the borders to foreground for binary erosions and closings. */
 	public static boolean padEdges;
-	/** Run the RMIListener (-1 unspecified, 0 no, 1 yes). */
-	public static int enableRMIListener = -1;
-	/** Only for backwards compatibility */
+	/** Run the SocketListener. */
 	public static boolean runSocketListener;
 
 	static Properties ijPrefs = new Properties();
@@ -149,6 +146,18 @@ public class Prefs {
 		loadOptions();
 		return null;
 	}
+
+	/*
+	static void dumpPrefs(String title) {
+		IJ.log("");
+		IJ.log(title);
+		Enumeration e = ijPrefs.keys();
+		while (e.hasMoreElements()) {
+			String key = (String) e.nextElement();
+			IJ.log(key+": "+ijPrefs.getProperty(key));
+		}
+	}
+	*/
 
 	static String loadAppletProps(InputStream f, Applet applet) {
 		if (f==null)
@@ -367,7 +376,7 @@ public class Prefs {
 		noRowNumbers = (options&NO_ROW_NUMBERS)!=0;
 		moveToMisc = (options&MOVE_TO_MISC)!=0;
 		pointAddToManager = (options&ADD_TO_MANAGER)!=0;
-		enableRMIListener = getInt(ENABLE_RMI, -1);
+		runSocketListener = (options&RUN_SOCKET_LISTENER)!=0;
 	}
 
 	static void saveOptions(Properties prefs) {
@@ -381,10 +390,8 @@ public class Prefs {
 			+ (noPointLabels?NO_POINT_LABELS:0) + (noBorder?NO_BORDER:0)
 			+ (showAllSliceOnly?SHOW_ALL_SLICE_ONLY:0) + (copyColumnHeaders?COPY_HEADERS:0)
 			+ (noRowNumbers?NO_ROW_NUMBERS:0) + (moveToMisc?MOVE_TO_MISC:0)
-			+ (pointAddToManager?ADD_TO_MANAGER:0);
+			+ (pointAddToManager?ADD_TO_MANAGER:0) + (runSocketListener?RUN_SOCKET_LISTENER:0);
 		prefs.put(OPTIONS, Integer.toString(options));
-		if (enableRMIListener >= 0)
-			prefs.put(ENABLE_RMI, new Integer(enableRMIListener));
 	}
 
 	/** Saves the value of the string <code>text</code> in the preferences
@@ -493,7 +500,7 @@ public class Prefs {
 	public static void savePrefs(Properties prefs, String path) throws IOException{
 		FileOutputStream fos = new FileOutputStream(path);
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
-		prefs.store(bos, "ImageJA "+ImageJ.VERSION+" Preferences");
+		prefs.store(bos, "ImageJ "+ImageJ.VERSION+" Preferences");
 		bos.close();
 	}
 	
