6f24c4a85171740987c65006ef916420cc132ecb
johannes.schindelin <johannes.schindelin@be23795f-a60e-40d4-ba19-055798872e57>
2010-06-17 08:16:07 +0000

    Merge remote branch 'origin/mob'
    Conflicts:
    	ij/ImageStack.java
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@990 be23795f-a60e-40d4-ba19-055798872e57

--- ij/ImageStack.java
+++ ij/ImageStack.java
@@ -40,7 +40,7 @@ public class ImageStack {
 		nSlices = size;
 	}
 
-	/** Creates a new, empty image stack. */
+	/** Creates a new, empty image stack using the specified color model. */
 	public ImageStack(int width, int height, ColorModel cm) {
 		this.width = width;
 		this.height = height;
@@ -57,12 +57,12 @@ public class ImageStack {
 		if (!pixels.getClass().isArray()) 
 			throw new IllegalArgumentException("'pixels' is not an array");
 		nSlices++;
-		if (nSlices==stack.length) {
+		if (nSlices>=stack.length) {
 			Object[] tmp1 = new Object[nSlices*2];
-			System.arraycopy(stack, 0, tmp1, 0, nSlices);
+			System.arraycopy(stack, 0, tmp1, 0, stack.length);
 			stack = tmp1;
 			String[] tmp2 = new String[nSlices*2];
-			System.arraycopy(label, 0, tmp2, 0, nSlices);
+			System.arraycopy(label, 0, tmp2, 0, label.length);
 			label = tmp2;
 		}
 		stack[nSlices-1] = pixels;
