7907e9fa8b1683b54e22788f3cd2bafdffbdc057
gharris <gharris@be23795f-a60e-40d4-ba19-055798872e57>
2010-10-14 13:40:59 +0000

    Implementations for AWT / Swing SDI and MDI modes
    git-svn-id: http://code.imagej.net/svn/imagej/branches/ijx@1547 be23795f-a60e-40d4-ba19-055798872e57

---
+++ imagej/src/main/java/implementation/mdi/WindowMDI.java
@@ -0,0 +1,36 @@
+package implementation.mdi;
+
+import implementation.swing.*;
+import ijx.gui.IjxWindow;
+import java.awt.Image;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author GBH
+ */
+public class WindowMDI extends JInternalFrame implements IjxWindow{
+
+  private boolean closed;
+    @Override
+    public boolean isClosed() {
+      return closed;
+    }
+
+    @Override
+    public boolean canClose() {
+      return true;
+    }
+
+    @Override
+    public boolean close() {
+        throw new UnsupportedOperationException("Not supported yet."); // @todo
+    }
+
+    public Image getIconImage() {
+        // @todo
+        return null;
+    }
+
+}
